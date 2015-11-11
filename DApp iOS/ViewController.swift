1f4b64fdc42da0b54db8f42f3bc6bc2631355fb7
Katie Koebrich <katiekoebrich@katiekoebrichmacbookpro.local>
2015-11-10 19:59:08 -0600

    Initial Commit

---
+++ DApp iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DApp iOS
+//
+//  Created by Katie Koebrich on 11/10/15.
+//  Copyright © 2015 Katie Koebrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
